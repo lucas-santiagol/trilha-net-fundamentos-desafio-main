7abd82e97e79a5926532aad46cc89a4e181c38f1
lucas santiago alcantara de oliveira <lsao@poli.br>
2024-01-07 17:40:10 -0300

    removendo cometários

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,12 +14,11 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+           
             Console.WriteLine("Digite a placa do veículo para estacionar: ");
-            //Pega o valor do ReadLine e armazena na variável do tipo string
+           
             string placa = Console.ReadLine();
-            //Adiciona na lista de veiculos o valor da variável placa
+            
             veiculos.Add(placa);
         }
 
@@ -27,20 +26,17 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover: ");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
+            
             string placa = "";
 
             placa = Console.ReadLine();
 
-            // Verifica se o veículo existe
+            
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+               
                 int horas = 0;
                 decimal valorTotal = 0; 
 
@@ -48,8 +44,7 @@ namespace DesafioFundamentos.Models
 
                 valorTotal = precoInicial + (precoPorHora * horas);
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                
 
                 veiculos.Remove(placa);
 
@@ -63,12 +58,11 @@ namespace DesafioFundamentos.Models
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
+            
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
 
                 foreach (string veiculo in veiculos)
                 {
